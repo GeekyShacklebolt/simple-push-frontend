514494f9961882a3bfc2da9552c359613024256e
Shiva Saxena <shivasaxena911@gmail.com>
2022-08-16 18:48:19 +0530

    feat(js): subscribe user to push notification

--- static/js/main.js
+++ static/js/main.js
@@ -20,6 +20,24 @@ function makeHttpRequest(method, url, body, headers) {
     }).catch((error) => logError(error));
 }
 
+function getServerPublicKey() {
+    return 'BE1Os0J-cotQcRHDsGB8qjlF27SUlQxJlOySNMrNN-jGvFW4W6k7ZxmqSxUcDwayR68gwSaGTUw1N-9scTwBK1s'
+}
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 
 // Event Listeners
 subscribeButton.addEventListener('click', function(event){
@@ -29,7 +47,7 @@ subscribeButton.addEventListener('click', function(event){
 
 notificationForm.addEventListener('submit', function(event){
     event.preventDefault();
-    createNotification(notificationForm)
+    createNewNotification(notificationForm)
     .then(function(result) {
         console.log(result)
     }).catch((error) => logError(error));
@@ -37,7 +55,7 @@ notificationForm.addEventListener('submit', function(event){
 
 
 // Core Functions
-function createNotification(notificationForm){
+function createNewNotification(notificationForm){
     const formInputs = notificationForm.elements
     let url = getBaseUrl() + '/notifications'
     let body = JSON.stringify({
@@ -60,14 +78,16 @@ function registerServiceWorker() {
             navigator.serviceWorker.register('./static/js/service-worker.js')
                 .then(function (registration) {
                     console.log('Service worker successfully registered!');
-                    return registration;
+                    return resolve(registration)
                 }).catch(function (err) {
                     console.error('Unable to register service worker.', err);
+                    return resolve(err)
                 });
         } else {
-            console.error('The browser does not support service workers or push messages.');
+            let msg = 'The browser does not support service workers or push messages.'
+            console.error(msg);
+            return reject(msg)
         }
-        throw new Error("Service worker registration failed!")
     })
 }
 
@@ -79,9 +99,27 @@ function requestNotificationPermission() {
     .then(function (permissionResult) {
         if (permissionResult === 'granted') {
             console.log("Notification Permission granted!")
-            console.log("TODO: subscribe user to push notification!")
+            return executeUserSubscriptionFlow()
         } else {
             throw new Error("Notification Permission NOT granted.")
         }
     }).catch((error) => logError(error))
+}
+
+function executeUserSubscriptionFlow() {
+    registerServiceWorker()
+    .then(function (registration) {
+        return subscribeUserToPushNotification(registration)
+    }).then(function (pushSubscription) {
+        console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+        console.log("TODO: Send push subscription details to Backend.")
+    }).catch((error) => logError(error))
+}
+
+function subscribeUserToPushNotification(registration) {
+    const subscribeOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(getServerPublicKey()),
+    };
+    return registration.pushManager.subscribe(subscribeOptions);
 }
