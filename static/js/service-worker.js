61889ff045b33b770c413e586553d99bf912caac
Shiva Saxena <shivasaxena911@gmail.com>
2022-08-22 13:12:19 +0530

    feat: make notification sending configurable (#8)
    * feat: make notification sending configurable
    * fix(service-worker): parse title and body from event data

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,4 +1,18 @@
+
+// Event Listeners
 self.addEventListener('push', function(event) {
-    const promiseChain = self.registration.showNotification(event.data.text());
+    const notification_data = event.data.json()
+    const options = parseOptionsFromNotificationData(notification_data)
+    const promiseChain = self.registration.showNotification(
+        notification_data["title"],
+        options,
+        );
     event.waitUntil(promiseChain);
 });
+
+// Utilities
+function parseOptionsFromNotificationData(notificationData) {
+    return {
+        "body": notificationData["description"]
+    };
+}
