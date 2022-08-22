c482eaf776d8378759b3c6839a71bc561234728e
Shiva Saxena <shivasaxena911@gmail.com>
2022-08-22 13:06:04 +0530

    fix(service-worker): parse title and body from event data

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,7 +1,18 @@
+
+// Event Listeners
 self.addEventListener('push', function(event) {
+    const notification_data = event.data.json()
+    const options = parseOptionsFromNotificationData(notification_data)
     const promiseChain = self.registration.showNotification(
-        title=event.data.text(),
-        options={"body": "test"},
+        notification_data["title"],
+        options,
         );
     event.waitUntil(promiseChain);
 });
+
+// Utilities
+function parseOptionsFromNotificationData(notificationData) {
+    return {
+        "body": notificationData["description"]
+    };
+}
